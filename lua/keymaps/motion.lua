d62e9e0cf9130eb6de3112cdfb2e496cb9187a77
lejeunel <me@lejeunel.org>
2024-09-29 05:55:07 +0200

    custom noice

--- lua/keymaps/motion.lua
+++ lua/keymaps/motion.lua
@@ -4,4 +4,10 @@ return {
   vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>'),
   vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>'),
   vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>'),
+
+  -- Automatically center screen on scroll
+  vim.keymap.set("n", "<C-d>", "<C-d>zz"),
+  vim.keymap.set("n", "<C-u>", "<C-u>zz"),
+  vim.keymap.set("n", "n", "nzzzv"),
+  vim.keymap.set("n", "N", "Nzzzv"),
 }
--- lua/keymaps/others.lua
+++ lua/keymaps/others.lua
@@ -1,10 +1,5 @@
-
+print('tmux sessionizer')
 return {
   vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>"),
 
-  -- Automatically center screen on scroll
-  vim.keymap.set("n", "<C-d>", "<C-d>zz"),
-  vim.keymap.set("n", "<C-u>", "<C-u>zz"),
-  vim.keymap.set("n", "n", "nzzzv"),
-  vim.keymap.set("n", "N", "Nzzzv"),
 }
