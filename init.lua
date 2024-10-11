97c047b134a11a796802727cf7e73242b9fe5582
lejeunel <me@lejeunel.org>
2024-10-11 10:50:45 +0200

    lsp clangd client

--- init.lua
+++ init.lua
@@ -98,6 +98,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "j", "jzz")
+vim.keymap.set("n", "k", "kzz")
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
