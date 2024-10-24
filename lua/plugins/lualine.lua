14ddaa4052d2049a29f29fdeb31439571703cd0e
lejeunel <me@lejeunel.org>
2024-10-24 15:34:15 +0200

    simplified lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,7 +20,9 @@ return {
 
     return {
       options = {
-        theme = 'gruvbox-material',
+        theme = 'gruvbox',
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
         globalstatus = true,
         disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'starter' } },
       },
